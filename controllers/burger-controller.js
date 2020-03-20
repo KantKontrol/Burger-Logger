08bb0392fe9f1f00fbf62756aa16f5c51031c8ba
KantKontrol <nick.derissio.tech@gmail.com>
2020-03-20 15:47:50 -0400

    connected html to backend with ajax

--- controllers/burger-controller.js
+++ controllers/burger-controller.js
@@ -6,5 +6,12 @@ module.exports = function(server){
     server.get("/", (req, res)=> {
         burger.selectAll(res);
     });
+
+    server.put("/:id", (req, res)=> {
+
+        let id = req.body.id;
+        console.log(id);
+        
+    });
 };
 

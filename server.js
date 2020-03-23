718f714d9b0f05758b2489ccdb222460d7972bd2
KantKontrol <nick.derissio.tech@gmail.com>
2020-03-23 15:33:11 -0400

    began modifying css of page, added burgers to bootstrap lists

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ const PORT = process.env.PORT || 8080;
 
 server.use(express.urlencoded({ extended: true }));
 server.use(express.json());
+server.use(express.static(path.join(__dirname, "/public")));
 
 
 server.engine("handlebars", exhbs({ defaultLayout: "main" }));

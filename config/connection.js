e0d5ece2b393d82c3b532e1fd7dd2af88682465e
KantKontrol <nick.derissio.tech@gmail.com>
2020-03-24 14:01:26 -0400

    added heroku db

--- config/connection.js
+++ config/connection.js
@@ -1,20 +1,13 @@
 const mysql = require("mysql");
 require("dotenv").config();
 
-let connection = mysql.createConnection({
-    host: 'localhost',
-    port: 3306,
-    user: process.env.USER_NAME,
-    password: process.env.USER_PASS,
-    database: "eatdaburger_db"
+const { Pool } = require('pg');
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: true
 });
 
-connection.connect((err, res)=> {
-    if(err)throw err;
+const connection = await pool.connect()
 
-    if(res){
-        console.log("Connected to db at " + connection.threadId);
-    }   
-});
 
 module.exports = connection;
